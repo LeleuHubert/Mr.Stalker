b03ff8ca8f468e397a5c6139e1702a889bbc958c
leleu hubert <hubert.leleu@epitech.eu>
2019-01-31 18:49:16 +0100

    Add my first discord Bot

---
+++ Mr.Stalker/Program.cs
@@ -0,0 +1,120 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mr.Stalker
+{
+    class Program
+    {
+
+        public  DiscordSocketClient Client;
+        private CommandService Commande;
+        public Random rnd;
+        private Thread spamThread;
+        public Talk.Spam spam;
+        public static Program P;
+        public static DateTime countdown;
+
+        static async Task Main(string[] args)
+        {
+            await new Program().MainAsync();    
+        }
+
+        async Task    MainAsync()
+        {
+            P = this;
+            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = Discord.LogSeverity.Verbose
+            });
+
+            rnd = new Random();
+            countdown = DateTime.Now;
+            spam = null;
+            spamThread = new Thread(Talk.Talker);
+            spamThread.Start();
+            Commande = new CommandService();
+            await Commande.AddModuleAsync<Talk>(null);
+
+            Client.MessageReceived += Client_MessageReceived;
+
+            Client.Log += Client_Log;
+            Commande.Log += Commande_Log;
+            await Client.LoginAsync(TokenType.Bot, File.ReadAllText("key.txt"));
+            await  Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage arg)
+        {
+            SocketUserMessage SUM = arg as SocketUserMessage;
+
+            if (SUM == null && arg.Author.IsBot)
+                return;
+            if (spam != null && spam.User.Id == arg.Author.Id || (DateTime.Now.Subtract(DateTime.Now).TotalSeconds > 60000))
+                spam = null;
+            int pos = 0;
+            if (SUM.HasMentionPrefix(Client.CurrentUser, ref pos) || SUM.HasStringPrefix("#bot", ref pos))
+            {
+                ITextChannel chan = arg.Channel as ITextChannel;
+                if (chan != null)
+                {
+                    IGuildUser user = (await chan.Guild.GetUsersAsync()).FirstOrDefault((x) =>
+                    {
+                        string enterName = SUM.Content.Substring(pos, SUM.Content.Length - pos).ToLower();
+                        if (Talk.CleanName(x.Username.ToLower()) == enterName)
+                            return (true);
+                        return (!string.IsNullOrEmpty(x.Nickname) && Talk.CleanName(x.Nickname.ToLower()) == enterName);
+                    });
+                    if (user != null)
+                        spam = new Talk.Spam(user, chan);
+                }
+                SocketCommandContext context = new SocketCommandContext(Client, SUM);
+
+                await Commande.ExecuteAsync(context, pos, null);
+
+            }
+        }
+
+        private Task Commande_Log(LogMessage arg)
+        {
+            Client_Log(arg);
+            return Task.CompletedTask;
+        }
+
+        private System.Threading.Tasks.Task Client_Log(Discord.LogMessage arg)
+        {
+            var cc = Console.ForegroundColor;
+            switch (arg.Severity)
+            {
+                case LogSeverity.Critical:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine(arg);
+            Console.ForegroundColor = cc;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Mr.Stalker/Talk.cs
@@ -0,0 +1,78 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Mr.Stalker
+{
+    public class Talk : ModuleBase 
+    {
+        static List<string> TalkMe = new List<string>()
+        {
+           {"Bouge toi, on t'attend" },
+            {"Sors toi les doigts du cul et réponds"},
+            {"Réponds là !!!!"},
+            {"On t'attend, sois réactif"},
+            {"Réponds putain, j'aime pas attendre"},
+            {"Remue toi, feignasse"},
+            {"TFQ ? Sac à fioule, réponds"},
+            {"On t'attend là. Et j'aime pas ça"},
+            {"Réveille toi et réponds à la question !!!!!"},
+            {"Allez allez, hop hop hop"},
+            {"Remue ta graisse et réponds"},
+            {"Let's go bitch, reply right now"},
+            {"Hubert, c'est le meilleur"},
+            {"Schneller, schneller, gib deine Antwort"},
+            {"Move your ovaries and reply"},
+            {"Reply, reply, reply putain, reply, allez reply !!!!!! "},
+            {"Răspunde fiului ticălos murdar !!"},
+            {"J'aime pas attendre, alors réponds"},
+            {"отвечает грязный цыганский сын"},
+            {"Überschutz doit être ton unique Dieu !! Réponds MERDE"},
+            {"Fais pas genre t'as une vie sociale et réponds !!"},
+            {"Tes seuls amis sont les futurs clients d'Übercshutz donc bouge toi !!!!"},
+            {"On te pose une question, TU RÉPONDS !!! "},
+            {"WOUUUAAAAAA ça me saoul réponds là"},
+            {"Même ma grand mére repond plus vite, et elle est morte !!!"},
+            {"STARFOULA tu réponds ou je t'inscris sur des sites pedoporno sur le deepweb !!!!! "}
+
+        };
+
+        public static string CleanName(string name)
+        {
+            string tmp = "";
+
+            foreach (char c in name)
+            {
+                if (char.IsLetter(c))
+                    tmp += c;
+            }
+
+            return tmp;
+        }
+
+        public class Spam
+        {
+            public Spam(IGuildUser user, ITextChannel chan)
+            {
+                User = user;
+                Chan = chan;
+            }
+
+            public IGuildUser User;
+            public ITextChannel Chan;
+        }
+
+        public static async void Talker()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                if (Program.P.spam != null)
+                {
+                    await Program.P.spam.Chan.SendMessageAsync("<@" + Program.P.spam.User.Id + "> " + TalkMe[Program.P.rnd.Next(0, TalkMe.Count)]);
+                    Thread.Sleep(30000);
+                }
+            }
+        }
+    }
+}
